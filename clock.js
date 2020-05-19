b3848f68d14a913388fab2291d8e931dbba10c5c
tph00300 <tph00100@gmail.com>
2020-05-19 16:45:48 +0900

    make clock

--- clock.js
+++ clock.js
@@ -6,11 +6,13 @@ function getTime()
     const date=new Date();
     const minutes = date.getMinutes();
     const hours = date.getHours();
-    clockTitle.innerText=`${hours}:${minutes}`;
+    const seconds = date.getSeconds();
+    clockTitle.innerText=`${hours<=9 ? `0${hours}` : hours}:${minutes <= 9 ? `0${minutes}` : minutes}:${seconds <= 9 ? `0${seconds}`: seconds }`;
 }
 function init()
 {
     getTime();
+    setInterval(getTime,1000);
 }
 
 init();
